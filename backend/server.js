99c973e51cb9970a6117afd30f025e7b70f56842
oybek703 <hhoybek@gmail.com>
2020-11-13 14:56:15 +0500

    basic express server is implemented

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+require('dotenv').config();
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('API is running...');
+})
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}...`));
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,8 @@
-import React, {Fragment} from 'react';
+import React from 'react';
 import './index.css';
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import './bootstrap.min.css'
 import {Container} from "react-bootstrap";
 import {BrowserRouter as Router, Route} from 'react-router-dom'
 import HomeScreen from "./screens/HomeScreen";
