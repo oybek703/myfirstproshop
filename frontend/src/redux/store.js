0b7e75ae9de12cf492e441722118576f9c8bf514
oybek703 <hhoybek@gmail.com>
2021-01-04 19:10:34 +0500

    fixed redux devtools bug

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -18,7 +18,7 @@ const store = createStore(
     initialState,
     compose(
         applyMiddleware(thunk),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+        window.__REDUX_DEVTOOLS_EXTENSION__ ? window .__REDUX_DEVTOOLS_EXTENSION__() : f => f
     )
 );
 
